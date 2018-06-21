4010fdcb9f4078e9a4b8a4ea61a02a4c0b5e2501
FCSM25110 <39257062+FCSM25110@users.noreply.github.com>
2018-06-21 10:57:20 +0200

    Add files via upload

---
+++ Programme_DeepSleep.ino
@@ -0,0 +1,15 @@
+
+
+unsigned long temps;
+float sec;
+int tim=1; // temps entre chaque reset = 1 min
+void setup(){
+  Serial.begin(9600);
+  Serial.println("5cara temps :"); 
+  temps = millis(); //la variable temps va prendre le nombre de millisecondes depuis que la carte est allumée
+  sec = temps/1000;
+  Serial.println(sec);  // On affiche le temps qu'il a fallu à la carte avant d'effectuer le reset
+}
+void loop(){   //prints time since program started
+  ESP.deepSleep((tim *60)*1000000L); // Le module passe en mode deepsleep et se reveillera toutes les minutes
+}
