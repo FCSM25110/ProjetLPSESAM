025a0f40e6c323429f492dea1347b3bb4540f617
FCSM25110 <39257062+FCSM25110@users.noreply.github.com>
2018-06-21 11:15:25 +0200

    Add files via upload

---
+++ Programme_batterie.ino
@@ -0,0 +1,22 @@
+int bBat=A0;
+float Btest;
+int pct;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(bBat,OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Btest=analogRead(bBat);
+pct=(Btest/1024)*100;
+Serial.println(Btest);
+Serial.println("Le niveau de batterie est :");
+Serial.print(pct);
+Serial.println("%");
+delay(2000);
+
+}
